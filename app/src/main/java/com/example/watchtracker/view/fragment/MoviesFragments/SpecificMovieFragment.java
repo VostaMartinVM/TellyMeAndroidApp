aa3d16dd24ea6c23cd3ae1a1ffaa42ae5d8d8ae0
VostaMartinVM <74717642+VostaMartinVM@users.noreply.github.com>
2022-04-18 10:08:49 +0200

    Fragment & ViewModels Architecture

---
+++ app/src/main/java/com/example/watchtracker/view/fragment/MoviesFragments/SpecificMovieFragment.java
@@ -0,0 +1,39 @@
+package com.example.watchtracker.view.fragment.MoviesFragments;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.watchtracker.R;
+import com.example.watchtracker.viewModel.MoviesFragments.SpecificMovieViewModel;
+
+public class SpecificMovieFragment extends Fragment {
+
+    private SpecificMovieViewModel mViewModel;
+
+    public static SpecificMovieFragment newInstance() {
+        return new SpecificMovieFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.specific_movie_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SpecificMovieViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
