667e77812889452f6a035183f424166ec9a722e4
VostaMartinVM <74717642+VostaMartinVM@users.noreply.github.com>
2022-04-19 16:29:11 +0200

    User fragment layout & movies and shows layout

--- app/src/main/java/com/example/watchtracker/view/fragment/ShowsFragments/ShowsFragment.java
+++ app/src/main/java/com/example/watchtracker/view/fragment/ShowsFragments/ShowsFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.watchtracker.viewModel.ShowsFragments.ShowsViewModel;
 import com.example.watchtracker.R;
@@ -26,7 +28,8 @@ public class ShowsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.shows_fragment, container, false);
+        View view = inflater.inflate(R.layout.shows_fragment, container, false);
+        return view;
     }
 
     @Override
