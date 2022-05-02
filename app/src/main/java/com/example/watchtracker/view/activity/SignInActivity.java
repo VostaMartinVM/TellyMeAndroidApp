061ebbe8313113e0ff13c06e250ca72594180604
HansLongLe <HansLongLe@gmail.com>
2022-05-02 17:36:48 +0200

    googleAuth

--- app/src/main/java/com/example/watchtracker/view/activity/SignInActivity.java
+++ app/src/main/java/com/example/watchtracker/view/activity/SignInActivity.java
@@ -1,16 +1,56 @@
 package com.example.watchtracker.view.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.watchtracker.Authentication.AuthWithGoogle;
 import com.example.watchtracker.R;
+import com.example.watchtracker.network.GoogleClient;
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private GoogleClient googleClient;
+    private AuthWithGoogle authWithGoogle;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        googleLogIn();
+        CallbackManager mCallBackManager = CallbackManager.Factory.create();
+        FloatingActionButton facebookButton = findViewById(R.id.facebook_authentication_button);
+
+    }
+
+    private void googleLogIn()
+    {
+        googleClient = GoogleClient.getInstance(getApplicationContext());
+        authWithGoogle = new AuthWithGoogle();
+        FloatingActionButton google = findViewById(R.id.google_authentication_button);
+        google.setOnClickListener(view -> {
+            signIn();
+        });
+    }
+
+    @SuppressWarnings("deprecation")
+    private void signIn()
+    {
+        Intent signInIntent = googleClient.getGoogleSignInClient().getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        authWithGoogle.activityResult(this, requestCode, data, RC_SIGN_IN);
+
     }
 }
